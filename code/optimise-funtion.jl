5b24f242c774796d3c06419c07352ccd2729a5a6
Leanne Massie <lm3015@ic.ac.uk>
2019-04-16 20:17:50 +0100

    working optimiser and function to optimise growth rate almost working

--- code/optimise-funtion.jl
+++ code/optimise-funtion.jl
@@ -5,49 +5,56 @@
 
 
 
-function gamma_finder(params, y)#input should be three arrays, the first with the 8 parameter values that 
+function gamma_finder(one,two,three,four,five,six,seven,eight,nine,
+    s_out,rmr_0,em_0,rmq_0,rmt_0,et_0,rmm_0,
+    mt_0,mm_0,q_0,si_0,mq_0,mr_0,r_0,
+    NH4_0,nit_mrna_0,nit_mrna_ribo_0,nit_0,exported_0,N_0,a_0, AA_0, 
+    AA_prot_0,AA_mrna_0,AA_mrna_ribo_0)
+    #input should be three arrays, the first with the 8 parameter values that 
 #we are varying and the second array should be the current values of the 25 molecules
     
     
-    results = init 
+    results = [s_out,rmr_0,em_0,rmq_0,rmt_0,et_0,rmm_0,
+    mt_0,mm_0,q_0,si_0,mq_0,mr_0,r_0,
+    NH4_0,nit_mrna_0,nit_mrna_ribo_0,nit_0,exported_0,N_0,a_0, AA_0, AA_prot_0,AA_mrna_0,AA_mrna_ribo_0]
     #making an array to record the results in
 
-    k_ribo_a= params[1]
-    k_ribo_a_AA= params[2]
-    k_ribo_AA= params[3]
-    k_a= params[4]
-    k_cat_AA= params[5]
-    k_a_NH4= params[6]
-    k_NH4= params[7]
-    k_a_AA= params[8]
-    k_NH4_AA= params[9]
+    k_ribo_a= one#params[1]
+    k_ribo_a_AA= two#params[2]
+    k_ribo_AA= three#params[3]
+    k_a= four#params[4]
+    k_cat_AA= five#params[5]
+    k_a_NH4= six#params[6]
+    k_NH4= seven#params[7]
+    k_a_AA= eight#params[8]
+    k_NH4_AA= nine#params[9]
     #unpack the 9 enzyme kinetic parameters that i want to optimise
     
-    s_out = y[1] #external level of substrate, a proxy for plant growth state since more plant = more substrate
-    rmr= y[2]#num of ribosome/ribosomal mRNA complex molecules
-    em= y[3]#metabolic enzyme molecumes
-    rmq= y[4]#num of ribosome/housekeeping mRNA complex molecules
-    rmt= y[5]#num of ribosome/transporter protein mRNA complex molecules
-    et= y[6]#num of transporter enzyme molecules
-    rmm= y[7]#num of ribosome/metabolic enzyme mRNA complexes
-    mt= y[8]#num of particles of transporter protein mRNA
-    mm= y[9]#num of particles of metabolic enzyme mRNA
-    q= y[10]#num of housekeeping protein molecules
-    si= y[11]#num of substrate molecules inside the cell (internal)
-    mq= y[12]#num of mRNA coding for housekeeping proteins
-    mr= y[13]#num of mRNA coding for ribosomes
-    r= y[14]#num of free ribosomes
-    NH4 = y[15] #num of ammonia molecules fixed
-    nit_mrna = y[16]#num of molecules of nitrogen fixing protein
-    nit_mrna_ribo = y[17]#num of molecules of mRNA coding for nitrogen fixing protein
-    nit = y[18]#num of molecules of mRNA-ribosome complexes coding for nitrogenase
-    exported= y[19]#total amount of NH4 exported
-    N = y[20] #num of bacteria cells
-    a= y[21]#num of ATP (proxy for level of energy)
-    AA = y[22] #num of amino acid molecules
-    AA_prot=y[23]#num of protein molecules that make new amino acids
-    AA_mrna=y[24]#num of mRNA coding for AA making protein
-    AA_mrna_ribo=y[25]#num of mRNA coding for AA making protein bound to ribosomes
+#~     s_out = y[1] #external level of substrate, a proxy for plant growth state since more plant = more substrate
+#~     rmr= y[2]#num of ribosome/ribosomal mRNA complex molecules
+#~     em= y[3]#metabolic enzyme molecumes
+#~     rmq= y[4]#num of ribosome/housekeeping mRNA complex molecules
+#~     rmt= y[5]#num of ribosome/transporter protein mRNA complex molecules
+#~     et= y[6]#num of transporter enzyme molecules
+#~     rmm= y[7]#num of ribosome/metabolic enzyme mRNA complexes
+#~     mt= y[8]#num of particles of transporter protein mRNA
+#~     mm= y[9]#num of particles of metabolic enzyme mRNA
+#~     q= y[10]#num of housekeeping protein molecules
+#~     si= y[11]#num of substrate molecules inside the cell (internal)
+#~     mq= y[12]#num of mRNA coding for housekeeping proteins
+#~     mr= y[13]#num of mRNA coding for ribosomes
+#~     r= y[14]#num of free ribosomes
+#~     NH4 = y[15] #num of ammonia molecules fixed
+#~     nit_mrna = y[16]#num of molecules of nitrogen fixing protein
+#~     nit_mrna_ribo = y[17]#num of molecules of mRNA coding for nitrogen fixing protein
+#~     nit = y[18]#num of molecules of mRNA-ribosome complexes coding for nitrogenase
+#~     exported= y[19]#total amount of NH4 exported
+#~     N = y[20] #num of bacteria cells
+#~     a= y[21]#num of ATP (proxy for level of energy)
+#~     AA = y[22] #num of amino acid molecules
+#~     AA_prot=y[23]#num of protein molecules that make new amino acids
+#~     AA_mrna=y[24]#num of mRNA coding for AA making protein
+#~     AA_mrna_ribo=y[25]#num of mRNA coding for AA making protein bound to ribosomes
     
     
     gam= (gmax*a*AA)/(k_ribo_a*k_ribo_a_AA+k_ribo_a_AA*a+k_ribo_AA_a*AA+a*AA)
@@ -106,8 +113,8 @@ function gamma_finder(params, y)#input should be three arrays, the first with th
     end_lam= end_ttrate/M
 
 
-    output = [end_lam, results]
-    return(output)
+#~     output = [end_lam, results]
+    return(end_lam)
 
 
     end  
--- code/optimiser.jl
+++ code/optimiser.jl
@@ -1,7 +1,7 @@
 ##parameter optimisation for AA model##
 
 using JuMP
-using GLPK
+#~ using GLPK
 using Ipopt
 
 #~ modelAA = Model(with_optimizer(GLPK.Optimizer))
@@ -11,16 +11,65 @@ modelAA = Model(with_optimizer(Ipopt.Optimizer))
 #the end of the burn in phase
 #~ [1.0e11, 4192.83, 333.316, 1624.11, 7.45172, 333.316, 7.45172, 24.5484, 24.5484, 72646.5, 143.077, 5350.34, 4646.88, 1.0342, 1000.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.59708e8, 12757.5, 72474.6, 5337.69, 1620.26]
 
-gmax = 1260.0
-a = 1.59708e8
-AA = 1000.0
+#~ gmax = 1260.0
+#~ a = 1.59708e8
+#~ AA = 1000.0
+
+rmr_0= 0.0
+em_0= 0.0
+rmq_0= 0.0
+rmt_0= 0.0
+et_0= 0.0
+rmm_0= 0.0
+mt_0= 0.0
+mm_0= 0.0
+q_0= 0.0
+si_0= 0.0
+mq_0= 0.0
+mr_0= 0.0
+r_0= 10.0
+a_0= 1000.0
+NH4_0 = 1000.0 #num of ammonia particles fixed by nitrogenase
+nit_0 = 0.0 #num nitrogenase proteins
+nit_mrna_0 = 0.0 #num of nitrogenase coding mRNA molecules
+nit_mrna_ribo_0 = 0.0#num of nitrogenase mRNA-ribosome complexes
+s_out = 10e10 #external substrate
+exported_0= 0.0#total amount of NH4 exported
+N_0 = 1.0 #num of bacteria cells to start with
+AA_0 = 20000.0#9.6e8 #num of amino acids to start with
+AA_prot_0 = 1.0
+AA_mrna_0=0.0
+AA_mrna_ribo_0=0.0
+
+init= [s_out,rmr_0,em_0,rmq_0,rmt_0,et_0,rmm_0,
+    mt_0,mm_0,q_0,si_0,mq_0,mr_0,r_0,
+    NH4_0,nit_mrna_0,nit_mrna_ribo_0,nit_0,exported_0,N_0,a_0, AA_0, AA_prot_0,AA_mrna_0,AA_mrna_ribo_0]
 
 
-@variable(modelAA, 0 <= k_ribo_a)
-@variable(modelAA, 0 <= k_ribo_a_AA)
-@variable(modelAA, 0 <= k_ribo_AA_a)
-@NLobjective(modelAA, Max, (gmax*a*AA)/(k_ribo_a*k_ribo_a_AA+k_ribo_a_AA*a+k_ribo_AA_a*AA+a*AA))
-@NLconstraint(modelAA, con1, k_ribo_a + k_ribo_a_AA + k_ribo_AA_a >= 1)
+@variable(modelAA, 0 <= k_ribo_a_var)
+@variable(modelAA, 0 <= k_ribo_a_AA_var)
+@variable(modelAA, 0 <= k_ribo_AA_a_var)
+@variable(modelAA, 0 <= k_a_var)
+@variable(modelAA, 0 <= k_cat_AA_var)
+@variable(modelAA, 0 <= k_NH4_var)
+@variable(modelAA, 0 <= k_a_NH4_var)
+@variable(modelAA, 0 <= k_a_AA_var)
+@variable(modelAA, 0 <= k_NH4_AA_var)
+
+
+#~ params = [k_ribo_a_var,k_ribo_a_AA_var,k_ribo_AA_a_var,k_a_var,k_cat_AA_var,k_NH4_var,k_a_AA_var,k_NH4_AA_var]
+#~ @variable(modelAA, 0<= params[1:9])
+
+include("optimise-funtion.jl")
+JuMP.register(modelAA, :gamma_finder, 33, gamma_finder, autodiff = true)
+
+@NLobjective(modelAA, Max, gamma_finder(k_ribo_a_var,k_ribo_a_AA_var,k_ribo_AA_a_var,
+    k_a_var,k_cat_AA_var,k_NH4_var,k_a_AA_var,k_NH4_AA_var,
+    s_out,rmr_0,em_0,rmq_0,rmt_0,et_0,rmm_0,mt_0,mm_0,q_0,si_0,mq_0,mr_0,r_0,
+    NH4_0,nit_mrna_0,nit_mrna_ribo_0,nit_0,exported_0,N_0,a_0, AA_0, AA_prot_0,AA_mrna_0,AA_mrna_ribo_0))
+
+#~ @NLobjective(modelAA, Max, (gmax*a*AA)/(k_ribo_a*k_ribo_a_AA+k_ribo_a_AA*a+k_ribo_AA_a*AA+a*AA))
+#~ @NLconstraint(modelAA, con1, k_ribo_a + k_ribo_a_AA + k_ribo_AA_a >= 1)
 println("model set up")
 #~ @variable(modelAA, 0<= x ) #create a jump variable called x with a lower bound at zero and upper at 2
 #~ @variable(model1, 0<= y ) #create a second variable y with its own bounds
@@ -41,28 +90,3 @@ println("optimal k_ribo_AA value is $AA_end")
 
 print((gmax*a*AA)/(a_end*a_AA_end+a_AA_end*a+AA_end*AA+a*AA))
 
-#~ @variable(modelAA, 0<= s_out )
-#~ @variable(modelAA, 0<= rmr )
-#~ @variable(modelAA, 0<= em )
-#~ @variable(modelAA, 0<= rmq )
-#~ @variable(modelAA, 0<= rmt )
-#~ @variable(modelAA, 0<= et )
-#~ @variable(modelAA, 0<= rmm )
-#~ @variable(modelAA, 0<= mt )
-#~ @variable(modelAA, 0<= mm )
-#~ @variable(modelAA, 0<= q )
-#~ @variable(modelAA, 0<= si )
-#~ @variable(modelAA, 0<= mq )
-#~ @variable(modelAA, 0<= mr )
-#~ @variable(modelAA, 0<= r )
-#~ @variable(modelAA, 0<= NH4 )
-#~ @variable(modelAA, 0<= nit_mrna )
-#~ @variable(modelAA, 0<= nit_mrna_ribo )
-#~ @variable(modelAA, 0<= nit )
-#~ @variable(modelAA, 0<= exported )
-#~ @variable(modelAA, 0<= N )
-#~ @variable(modelAA, 0<= a )
-#~ @variable(modelAA, 0<= AA )
-#~ @variable(modelAA, 0<= AA_prot )
-#~ @variable(modelAA, 0<= AA_mrna )
-#~ @variable(modelAA, 0<= AA_mrna_ribo )
